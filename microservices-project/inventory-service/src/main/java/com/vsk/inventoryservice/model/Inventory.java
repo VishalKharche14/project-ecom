32c3653c26b01876d09452e37391b4915ff267a5
Vishal <mh20bu3372@gmail.com>
2023-01-19 17:39:41 +0530

    Initial Project Files

---
+++ microservices-project/inventory-service/src/main/java/com/vsk/inventoryservice/model/Inventory.java
@@ -0,0 +1,23 @@
+package com.vsk.inventoryservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="t_invetory")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Inventory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String skuCode;
+    private Integer quantity;
+
+
+}
---
+++ microservices-project/order-service/src/main/java/com/vsk/orderservice/dto/OrderRequest.java
@@ -0,0 +1,13 @@
+package com.vsk.orderservice.dto;
+
+import lombok.*;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class OrderRequest {
+    private List<OrderLineItemsDto> orderLineItemsDtoList ;
+}

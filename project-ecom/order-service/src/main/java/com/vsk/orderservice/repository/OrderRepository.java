b5236f3bfdb4976d590ece50ebd7c2fb99810ca2
Vishal <mh20bu3372@gmail.com>
2023-01-20 00:25:23 +0530

    rebalanced

--- project-ecom/order-service/src/main/java/com/vsk/orderservice/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vsk.orderservice.repository;
-
-import com.vsk.orderservice.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-
-}
--- project-ecom/product-service/src/main/java/com/vsk/productservice/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.vsk.productservice.repository;
-
-import com.vsk.productservice.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-
-}

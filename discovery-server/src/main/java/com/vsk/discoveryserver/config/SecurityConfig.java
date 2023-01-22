21536805a06db313ad8881fe2e0732d20edd8169
Vishal <mh20bu3372@gmail.com>
2023-01-22 18:14:28 +0530

    WebSecurityConfigurerAdapter not available

---
+++ discovery-server/src/main/java/com/vsk/discoveryserver/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.vsk.discoveryserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable()
+                .authorizeRequests().anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.inMemoryAuthentication()
+                .withUser("eureka").password("password")
+                .authorities("USER");
+
+    }
+}
